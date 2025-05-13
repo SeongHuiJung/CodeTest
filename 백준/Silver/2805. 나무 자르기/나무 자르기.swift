aeab787a96cd6f2eba3b5f153e0ec866665664c8
SeongHuiJung <105810571+SeongHuiJung@users.noreply.github.com>
2025-05-14 00:06:35 +0900

    [Silver II] Title: 나무 자르기, Time: 560 ms, Memory: 155816 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.swift
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.swift
@@ -6,31 +6,20 @@ let M = NM[1]
 
 let trees = readLine()!.split{$0==" "}.map{Int(String($0))!}.sorted(by: <)
 let max = trees.last!
-let min = trees.first!
 
-print(binarySearch(left: 0, right: max, last: 0))
 
-func binarySearch(left: Int, right: Int, last: Int) -> Int {
-    if left > right {
-        return last
-    }
+var left = 0
+var right = max
+while(left <= right) {
     
     let mid = (left + right) / 2
-    
     var sum = 0
     trees.forEach {
         if $0 - mid > 0 {
             sum += $0 - mid
         }
     }
-    
-    if M == sum {
-        return mid
-    }
-    else if M < sum {
-        return binarySearch(left: mid + 1, right: right, last: mid)
-    }
-    else {
-        return binarySearch(left: left, right: mid - 1, last: mid - 1)
-    }
+    if M > sum { right = mid - 1 }
+    else { left = mid + 1 }
 }
+print(left - 1)
