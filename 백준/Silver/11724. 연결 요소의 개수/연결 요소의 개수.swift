44593370a02095a8af03bb8ffb7ed3f5ddba7e63
SeongHuiJung <105810571+SeongHuiJung@users.noreply.github.com>
2025-05-24 20:57:07 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 620 ms, Memory: 88216 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.swift
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.swift
@@ -1,41 +1,33 @@
 import Foundation
-var NM = readLine()!.split{$0==" "}.map{Int(String($0))!}
+
+let NM = readLine()!.split{$0==" "}.map{Int(String($0))!}
 let N = NM[0]
 let M = NM[1]
-var map: [[Int]] = [[Int]](repeating: [], count: N)
-var visited = [Bool](repeating: false, count: N)
+var map = [[Int]](repeating: [], count: N + 1)
+var visited = [Bool](repeating: false, count: N + 1)
+var count = 0
+
 for i in 0..<M {
     let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
-    let uNode = input[0] - 1
-    let vNode = input[1] - 1
-    
-    map[uNode].append(vNode)
-    map[vNode].append(uNode)
+    map[input[0]].append(input[1])
+    map[input[1]].append(input[0])
 }
-var cnt = 0
-for i in 0..<N {
-    if visited[i] == false {
-        // 연결된 간선이 하나도 없는 경우
-        if map[i].isEmpty {
-            visited[i] = true
-            // 연결된 정점이 없더라도 노드 하나로서 하나의 그래프이므로 추가+1
-            cnt += 1
-        }
-        // 연결된 간선이 있는 경우
-        else {
-            dfs(u : i)
-            cnt += 1
-        }
+
+for i in 1...N {
+    if !visited[i] {
+        dfs(i)
+        count += 1
     }
 }
-print(cnt)
 
-func dfs(u : Int) {
-    visited[u] = true
-    for i in 0..<map[u].count {
-        if visited[map[u][i]] == false {
-            //visited[map[u][i]] = true
-            dfs(u: map[u][i])
+print(count)
+
+func dfs(_ idx: Int) {
+    visited[idx] = true
+    
+    for i in 0..<map[idx].count {
+        if !visited[map[idx][i]] {
+            dfs(map[idx][i])
         }
     }
 }
